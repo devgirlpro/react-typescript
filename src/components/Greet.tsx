3b75f6d752ebb44ea36dfee5212f3bbbfdf0315a
devgirlpro <agalidari@gmail.com>
2024-03-13 16:55:52 +0100

    one step back without props

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,12 +1,12 @@
 import { type } from '@testing-library/user-event/dist/type';
 import React from 'react';
- type GreetProps = {
-    name: string
-}
+//  type GreetProps = {
+//     name: string
+// }
 
 
-const Greet = (props: GreetProps) => {
-  return <h2>Welcome {props.name}! You have 10 unread message!</h2>;
+const Greet = () => {
+  return <h2>Welcome  Lili! You have 10 unread message!</h2>;
 };
 
 export default Greet;
